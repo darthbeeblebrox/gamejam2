f09dd97ed36d1b60efa6b535db2fed6af8eb63cc
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-02-13 14:44:56 +0100

    Fix collision box bug

--- Scripts/NPCShop.gd
+++ Scripts/NPCShop.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
-export var player_near_text = "Friends for Food!"
+export var player_not_enough_food_text = "Need more food!"
+export var player_enough_food_text = "Use food to recruit help!"
+
 
 func _on_NPCShop_body_entered(body):
 	if body.name == "Player":
 		body.can_recruit_friend = true
-		$Speech.start_text_bubble(player_near_text)
+		if ScoreSingleton.TOTAL_FOOD >= ScoreSingleton.FOOD_PER_FRIEND:
+			$Speech.start_text_bubble(player_enough_food_text)
+		else:
+			$Speech.start_text_bubble(player_not_enough_food_text)
 
 
 func _on_NPCShop_body_exited(body):
